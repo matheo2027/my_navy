909dc243b47806037bdbfde58ea0d20abfccb4d6
Matheo PIQUES <matheo.piques@epitech.eu>
2023-01-09 14:32:18 +0100

    fix bug -h

--- src/main.c
+++ src/main.c
@@ -11,10 +11,10 @@ int main(int ac, char **av)
 {
     if (ac < 2 || ac > 3) {
         write(2, "Error: incorrect number of "
-        "arguments given, retry with -h\n", 58);
+        "arguments given, retry with -h\n", 59);
         return 84;
     }
-    if (av[1][0] == '-' && av[1][1] == 'h') {
+    if (av[1][0] == '-' && av[1][1] == 'h' && av[1][2] == '\0') {
         h_flag();
     }
     return 0;
