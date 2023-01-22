5e98d089a7edef9684feccf80ad08fe19c8469c2
Alea <alea.chlodnik@epitech.eu>
2023-01-22 18:43:49 +0100

    [UPD] both.c

--- src/players/both.c
+++ src/players/both.c
@@ -19,15 +19,12 @@ void connect_handler(int sig_nb, siginfo_t *siginfo, void *ucontext)
     if (sig_nb == SIGUSR1) {
         sleep(1);
         my_printf("\nenemy connected\n");
-    } else {
-        write(2, "Error: connection failed\n", 26);
-        return;
     }
 }
 
 void get_sig(void)
 {
-    struct sigaction sig_act;
+    struct sigaction sig_act = {0};
     sig_act.sa_flags = SA_RESTART | SA_SIGINFO;
     sig_act.sa_sigaction = &connect_handler;
     sigaction(SIGUSR1, &sig_act, NULL);
